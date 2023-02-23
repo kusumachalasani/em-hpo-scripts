6dc1c679d5b6036b6a3a1faf0a7e448686478af8
kusumachalasani <kchalasa@redhat.com>
2023-02-23 10:08:49 +0530

    Script to aggegrate data for a workload
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- kruize-remotemonitoring/aggregateWorkloadMetrics.py
+++ kruize-remotemonitoring/aggregateWorkloadMetrics.py
@@ -2,14 +2,11 @@ import pandas as pd
 import os
 
 # Load the CSV file into a pandas DataFrame
-df = pd.read_csv('cop-0223.csv')
+df = pd.read_csv(inputfile)
 
 #Remove the rows if there is no owner_kind, owner_name and workload
 columns_to_check = ['owner_kind', 'owner_name', 'workload', 'workload_type']
 df = df.dropna(subset=columns_to_check, how='any')
-
-#df.to_csv('cop-ch.csv', index=False)
-#df = pd.read_csv('cop-ch.csv')
 
 # Create a column with k8_object_type
 df['k8_object_type'] = ''
@@ -21,9 +18,6 @@ for i, row in df.iterrows():
     else:
         df.at[i, 'k8_object_type'] = row['workload_type']
 
-#df.to_csv('cop-withobjType.csv', index=False)
-#df = pd.read_csv('cop-withobjType.csv')
-
 # Update k8_object_name based on the type and workload.
 df['k8_object_name'] = ''
 for i, row in df.iterrows():
@@ -33,16 +27,6 @@ for i, row in df.iterrows():
         df.at[i, 'k8_object_name'] = row['owner_name']
 
 df.to_csv('cop-withobjType.csv', index=False)
-
-
-# Create a temporary file to append the aggregate data from multiple files.
-# Extract the header row
-#header_row = df.columns.tolist()
-
-# Create a new DataFrame with no rows but with the same columns as the existing file
-#new_df = pd.DataFrame(columns=header_row)
-#new_df.to_csv('cop-agg.csv', index=False)
-
 
 # Specify the columns to sort by
 #sort_columns = ['namespace', 'k8_object_type', 'owner_name', 'image_name', 'container_name', 'interval_start'] 
@@ -64,25 +48,17 @@ for key, group in grouped:
     filename = f"file_{counter}.csv"
 #    filename = '_'.join(str(x) for x in key) + '.csv'
     filepath = os.path.join(output_dir, filename)
-    print(filepath)
     group.to_csv(filepath, index=False)
 
 
-#Create a temporary file to append the aggregate data from multiple files.
+#Create a temporary file with a header to append the aggregate data from multiple files.
 # Extract the header row
 header_row = df.columns.tolist()
-
-# Create a new DataFrame with no rows but with the same columns as the existing file
 agg_df = pd.DataFrame(columns=header_row)
-#new_df.to_csv('cop-agg.csv', index=False)
-
-#agg_df = pd.read_csv('cop-agg.csv')
 
 for filename in os.listdir(output_dir):
     if filename.endswith('.csv'):
         filepath = os.path.join(output_dir, filename)
-
-        # Read the CSV file into a pandas dataframe
         df = pd.read_csv(filepath)
        
         # Calculate the average and minimum values for specific columns
@@ -109,8 +85,6 @@ agg_df.to_csv('final.csv', index=False)
 # list of column names to ignore
 columns_to_ignore = ['pod', 'owner_name', 'node' , 'resource_id']
 df1 = pd.read_csv('final.csv')
-
-# Drop duplicates while ignoring the specified columns
 df1 = df1.drop_duplicates(subset=[col for col in df.columns if col not in columns_to_ignore])
 
 # Drop the columns which are not required (optional)
