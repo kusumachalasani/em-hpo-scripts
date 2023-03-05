d68fc646d6a148fd58755fef77f7730e095b706a
kusumachalasani <kchalasa@redhat.com>
2023-03-05 21:50:34 +0530

    Update csv2json.py

--- kruize-remotemonitoring/csv2json.py
+++ kruize-remotemonitoring/csv2json.py
@@ -16,72 +16,85 @@ def create_json_from_csv(csv_file_path):
         csvreader = csv.DictReader(csvfile)
 
         for row in csvreader:
-            container_metrics_all = {}
-            container_metrics = {}
+            container_metrics = []
+            
             if row["cpu_request_container_avg"]:
-                container_metrics["cpuRequest"] = {
-                        "results": {
-                            "aggregation_info": {
-                                "sum": float(row["cpu_request_container_sum"]),
-                                "avg": float(row["cpu_request_container_avg"]),
-                                "units": "cores"
-                                }
-                            }
-                        }
-            if row["cpu_limit_container_avg"]:
-                container_metrics["cpuLimit"] = {
-                        "results": {
-                            "aggregation_info": {
-                                "sum": float(row["cpu_limit_container_sum"]),
-                                "avg": float(row["cpu_limit_container_avg"]),
-                                "units": "cores"
-                                }
-                            }
-                        }
-            if row["cpu_throttle_container_max"]:
-                container_metrics["cpuThrottle"] = {
-                        "results": {
-                            "aggregation_info": {
-                                "sum": float(row["cpu_throttle_container_sum"]),
-                                "max": float(row["cpu_throttle_container_max"]),
-                                "avg": float(row["cpu_throttle_container_avg"]),
-                                "units": "cores"
-                                }
-                            }
-                        }
-            container_metrics["cpuUsage"] = {
+                container_metrics.append({
+                    "name": "cpuRequest",
                     "results": {
                         "aggregation_info": {
-                            "sum": float(row["cpu_usage_container_sum"]),
-                            "min": float(row["cpu_usage_container_min"]),
-                            "max": float(row["cpu_usage_container_max"]),
+                            "sum": float(row["cpu_request_container_sum"]),
+                            "avg": float(row["cpu_request_container_avg"]),
                             "units": "cores"
-                            }
                         }
-                    }            
-            if row["memory_request_container_avg"]:
-                container_metrics["memoryRequest"] = {
-                        "results": {
-                            "aggregation_info": {
-                                "sum": float(row["memory_request_container_sum"])/mebibyte,
-                                "avg": float(row["memory_request_container_avg"])/mebibyte,
-                                "units": "MiB"
-                                }
-                            }
-                        }
-            if row["memory_limit_container_avg"]:
-                container_metrics["memoryLimit"] = {
-                        "results": {
-                            "aggregation_info": {
-                                "sum": float(row["memory_limit_container_sum"])/mebibyte,
-                                "avg": float(row["memory_limit_container_avg"])/mebibyte,
-                                "units": "MiB"
-                                }
-                            }
-                        }
-            container_metrics["memoryUsage"] =  {
+                    }
+                })
+                
+            if row["cpu_limit_container_avg"]:
+                container_metrics.append({
+                    "name" : "cpuLimit",
                     "results": {
                         "aggregation_info": {
+                            "sum": float(row["cpu_limit_container_sum"]),
+                            "avg": float(row["cpu_limit_container_avg"]),
+                            "units": "cores"
+                        }
+                    }
+                })
+                
+            if row["cpu_throttle_container_max"]:
+                container_metrics.append({
+                    "name" : "cpuThrottle",
+                    "results": {
+                        "aggregation_info": {
+                            "sum": float(row["cpu_throttle_container_sum"]),
+                            "max": float(row["cpu_throttle_container_max"]),
+                            "avg": float(row["cpu_throttle_container_avg"]),
+                            "units": "cores"
+                        }
+                    }
+                })
+                
+            container_metrics.append({
+                "name" : "cpuUsage",
+                "results": {
+                    "aggregation_info": {
+                        "sum": float(row["cpu_usage_container_sum"]),
+                        "min": float(row["cpu_usage_container_min"]),
+                        "max": float(row["cpu_usage_container_max"]),
+                        "units": "cores"
+                    }
+                }
+            })            
+            
+            if row["memory_request_container_avg"]:
+                container_metrics.append({
+                    "name" : "memoryRequest",
+                    "results": {
+                        "aggregation_info": {
+                            "sum": float(row["memory_request_container_sum"])/mebibyte,
+                            "avg": float(row["memory_request_container_avg"])/mebibyte,
+                            "units": "MiB"
+                        }
+                    }
+                })
+                
+            if row["memory_limit_container_avg"]:
+                container_metrics.append({
+                    "name" : "memoryLimit",
+                    "results": {
+                        "aggregation_info": {
+                            "sum": float(row["memory_limit_container_sum"])/mebibyte,
+                            "avg": float(row["memory_limit_container_avg"])/mebibyte,
+                            "units": "MiB"
+                        }
+                    }
+                })
+                
+            container_metrics.append({
+                "name" : "memoryUsage",
+                "results": {
+                    "aggregation_info": {
                             "min": float(row["memory_usage_container_min"])/mebibyte,
                             "max": float(row["memory_usage_container_max"])/mebibyte,
                             "sum": float(row["memory_usage_container_sum"])/mebibyte,
@@ -89,27 +102,26 @@ def create_json_from_csv(csv_file_path):
                             "units": "MiB"
                         }
                     }
-                }
-            container_metrics["memoryRSS"] = {
-                    "results": {
-                        "aggregation_info": {
-                            "min": float(row["memory_rss_usage_container_min"])/mebibyte,
-                            "max": float(row["memory_rss_usage_container_max"])/mebibyte,
-                            "sum": float(row["memory_rss_usage_container_sum"])/mebibyte,
-                            "avg": float(row["memory_rss_usage_container_avg"])/mebibyte,
-                            "units": "MiB"
-                        }
+                })
+           
+            container_metrics.append({
+                "name" : "memoryRSS",
+                "results": {
+                    "aggregation_info": {
+                        "min": float(row["memory_rss_usage_container_min"])/mebibyte,
+                        "max": float(row["memory_rss_usage_container_max"])/mebibyte,
+                        "sum": float(row["memory_rss_usage_container_sum"])/mebibyte,
+                        "avg": float(row["memory_rss_usage_container_avg"])/mebibyte,
+                        "units": "MiB"
                     }
                 }
-            
-            if container_metrics:
-                container_metrics_all.update(container_metrics)
-           
+            })
+                      
             # Create a dictionary to hold the container information
             container = {
                 "image_name": row["image_name"],
                 "container_name": row["container_name"],
-                "container_metrics": container_metrics_all
+                "container_metrics": container_metrics
             }
             
             # Create a list to hold the containers
@@ -141,5 +153,3 @@ def create_json_from_csv(csv_file_path):
 
 filename = sys.argv[1]
 create_json_from_csv(filename)
-
-
